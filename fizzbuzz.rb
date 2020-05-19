43dd97aff9a37860fe3e487b0f85979aa9f9f686
SULAIMAN - <sulaiman@ce.student.pens.ac.id>
2020-05-19 05:05:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     "Fizz" # Go fizz
   end
 end
- 
+
 fizzbuzz(3) # => You should see a return of "Fizz"
 fizzbuzz(5) # => You should see a return of nil
 fizzbuzz()  # => You should get an ArgumentError
